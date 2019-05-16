585217dfc7628de0e15e22e5fc312a5dd8f1d845
einzigartigerName <nelson.gillo@gmx.de>
2019-05-16 13:29:15 +0200

    Fix: Typo in description

--- pe011.hs
+++ pe011.hs
@@ -1,5 +1,5 @@
 {-
-    Solution for Project Euler Problem 10
+    Solution for Project Euler Problem 11
     What is the greatest product of four adjacent numbers in
     the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
 -}
